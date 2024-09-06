16893c60843ad6ae52de3ed3384c4dcc1829b12b
Shehzeen Ali <shehzeenali12@gmail.com>
2024-09-06 14:06:00 +0500

    add files and folders

---
+++ app.js
@@ -0,0 +1,163 @@
+// Q1
+alert("Hi I'm a Web Developer");
+
+// Q2
+alert("Error! Please Enter a valid password");
+
+// Q3
+alert("Welcome to JS Land \n Happy Coding");
+
+// Q4
+alert("Welcome to JS Land");
+alert("Happy Coding \n Prevent this page from creative additional changes");
+
+// Q6
+console.log("Hello... I can run JS through my web browser's console")
+
+// Chap 2 Q1
+var username = ("shehzeenali");
+console.log(username);
+
+// Q2
+var myName = ("Shehzeen Ali");
+console.log(myName);
+
+// Q3
+var message = ("Hello World");
+alert(message);
+
+// Q4
+var name = ("Johne Doe");
+var age = ("15 year old");
+var occupation = ("Certified Mobile Application Development");
+alert(name);
+alert(age);
+alert(occupation);
+
+// Q5
+var pizza = ("Pizza \n Pizz \n Piz \n Pi \n P");
+alert(pizza);
+
+// Q6
+var email = ("shehzeenali12@gmail.com");
+alert("My email is " + email);
+
+
+// Q7
+var book = ("A smarter way to learn JavaScript");
+alert("Im trying to from the book " + book);
+
+// Q8
+document.write("Yah! I can write HTML content through JavaScript");
+
+// Q9
+var msg = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+alert(msg);
+
+
+// Chap 3 Q1
+var age;
+age = "15 years old";
+alert("Im " + age);
+
+// Q2
+var num;
+num = "14 times";
+num = "15 times";
+alert("You have visited this site " + num);
+
+// Q3
+var birthYear = 1990;
+document.write("My birth is " + birthYear + " Data type of Declared variable is number");
+
+// Q4
+var Name = "John Dohne";
+var title = "Xyz Clothing Store";
+var PieceNum = "5T-Shirts";
+document.write(Name + " ordered " + PieceNum + " on " + title);
+
+// Chap 4 Q1
+var variable1 = "Hello"; var variable2 = "Im"; var variable3 = "Shehzeen";
+document.write(variable1 , " " +variable2, " " +variable3);
+
+// Q2
+// LEGAL VAR NAMES
+// var fullName;
+// var full_Name;
+// var fullName$;
+// var fullName500;
+// var FULLNAME;
+
+// ILLEGAL VAR NAMES
+// var 2fullName;
+// var full name;
+// var full@name;
+// var full'name;
+// var $fulname%;
+
+// Q3
+var vr1 = "numbers";
+var vr2 = "$";
+var vr3 = "_";
+var vr4 = "letter";
+document.write("Variable name can only contain. " + vr1 + " , " + vr2 + " and " + vr3 + " . For example my_1stVariable");
+
+document.write("Variable must begin with a " + vr4 + " , " + vr2 + " and " + vr3 + " . For example $name, _name or name");
+
+// Word Docu Ques 
+// Chap 1 
+// Q1
+alert("Shehzeen");
+alert("Ali");
+alert("shehzeenali12@gmail.com");
+alert("03XX-XXXXXXX");
+
+// Q2
+alert("Im learning JavaScript");
+
+// Q3
+alert("Hello Js");
+
+
+// Chap 2
+// Q1
+var surName;
+
+// Q2
+var Message;
+var Message = "Hello! World!";
+
+// Q3
+var teamName = "Team Shehz";
+alert(teamName);
+
+// Q4
+var bestMan = "Charlie";
+bestMan = "Alex";
+
+// Chap 3
+// Q1
+var caseQty;
+
+// Q2
+var caseQty;
+caseQty = 144;
+
+// Q3
+var num = 9;
+
+// Q4
+var result;
+result = 4 + 3;
+console.log(result);
+
+// Q5
+var merchTotal = 100;
+var shippingCharge = 10;
+var orderTotal = merchTotal + shippingCharge;
+console.log(orderTotal);
+
+// Q6
+var Num = 36;
+Num = Num + 44;
+console.log(Num);
